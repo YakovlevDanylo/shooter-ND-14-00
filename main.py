9694637a1cf76ca50743afb9d322af167c332d4f
YakovlevDanylo <166537891+YakovlevDanylo@users.noreply.github.com>
2025-03-09 13:47:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ class Player(GameSprite):
         if keys[K_d] and self.rect.x < window_width - 80:
             self.rect.x += self.speed
 
+    def fire(self):
+        bullet = Bullet("bullet.png", self.rect.centerx, self.rect.top, 10, 20, 15)
+        bullets.add(bullet)
 class Enemy(GameSprite):
 
     def update(self):
@@ -34,11 +37,19 @@ class Enemy(GameSprite):
             self.rect.y = 0
             self.speed = randint(1, 5)
 
+class Bullet(GameSprite):
+
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y <= 0:
+            self.kill()
+
 window_width = 700
 window_height = 500
 
 lost = 0
 
+bullets = sprite.Group()
 enemies = sprite.Group()
 for i in range(5):
     rand_x = randint(0, window_width - 100)
@@ -57,6 +68,9 @@ mixer.init()
 mixer.music.load("space.ogg")
 mixer.music.play()
 
+fire_sound = mixer.Sound("fire.ogg")
+
+
 clock = time.Clock()
 fps = 60
 
@@ -66,6 +80,10 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+        if e.type == KEYDOWN and e.key == K_SPACE:
+            player.fire()
+            fire_sound.play()
+
 
     window.blit(background, (0, 0))
 
@@ -74,6 +92,9 @@ while run:
 
     enemies.update()
     enemies.draw(window)
+
+    bullets.update()
+    bullets.draw(window)
 
     display.update()
     clock.tick(fps)
